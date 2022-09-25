14e55029a4d4add86bac1f6266d60c7b270997c7
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-09-25 20:16:56 +0100

    setting up for tests

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='harlow_bindicator',
+      version='0.0.0',
+      description='',
+      long_description='',
+      author='Joseph McCarthy',
+      url='https://github.com/joseph-mccarthy/harlow-bindicator',
+      copyright='Copyright 2022 Joseph McCarthy',
+      license='MIT',
+      packages=['harlow_bindicator'],
+      install_requires=[]
+)
