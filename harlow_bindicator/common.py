7b613881e3a1e5abcbeb3689393fbb6488f8029b
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-10-07 12:51:56 +0100

    refactor into script and send mqtt message when run

--- harlow_bindicator/common.py
+++ harlow_bindicator/common.py
@@ -1,31 +1,23 @@
 def common_setup():
     import argparse
     import logging
-    import os
 
     parser = argparse.ArgumentParser(description="Get List of Bin Collections for UPRN")
     parser.add_argument(
         "--uprn", type=int, help="Unique Property Reference Number",
     )
-    parser.add_argument(
-        "--log", type=str, required=False, help="Log Level", default=logging.WARN
-    )
-
-    parser.add_argument('--phone_numbers', type=str, nargs='+')
-    
+   
     args = parser.parse_args()
 
     logging.basicConfig(
-        level=args.log,
+        level="INFO",
         format="%(asctime)s | %(levelname)-8s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
     if not args.uprn:
-        logging.warn("no UPRN provided checking system variables.")
-        args.uprn = os.getenv('UPRN')
-        if not args.uprn:
-            logging.error("exiting no UPRN provided unable to proceed.")
-            exit(1)
+        logging.error("exiting no UPRN provided unable to proceed.")
+        exit(1)
+            
 
     return args
--- harlow_bindicator/main.py
+++ harlow_bindicator/main.py
@@ -2,87 +2,49 @@ from data import CollectionDate
 from api import Api
 from common import common_setup
 import logging
-import pause
+import json
 from datetime import datetime
-from datetime import timedelta
-from gpiozero import LED
-from notifications import Twilio
+import paho.mqtt.client as paho
 
 
 class Bindicator:
 
     uprn: int
     api: Api
-    bin_type: str
-    bin_day: bool = False
 
-    phone_numbers = []
-
-    bin_day_led = LED(2)
-    recycling_bin_led = LED(3)
-    rubbish_bin_led = LED(4)
-    active_led = LED(26)
-
-    def __init__(self, uprn: int, phone_numbers) -> None:
+    def __init__(self, uprn: int) -> None:
         self.uprn = uprn
-        self.phone_numbers = phone_numbers
         self.api = Api(uprn)
-        logging.warning(f"running Bindicator for property {self.uprn}")
-        if phone_numbers:
-            logging.info(f"nn bin day will send notification to {self.phone_numbers}.")
+        logging.info(f"running Bindicator for property {self.uprn}")
 
     def run(self):
-        self.active_led.blink()
-        while True:
-            collections = self.api.get_data()
 
-            if collections:
-                collection = collections[0]
-                if self.__is_today_bin_day(collection):
-                    self.__configure_for_bin_day(collection)
-                    logging.warning(f"today is bin day for {self.bin_type}")
-                    if self.phone_numbers:
-                        logging.info("attempting to send notifications")
-                        twilio = Twilio(self.phone_numbers)
-                        twilio.send_notification(collection.wheelie)
+        collections = self.api.get_data()
 
-                else:
-                    logging.warning(f"today {datetime.now().date()} is not bin day")
-                    self.__turn_lights_off()
-                    self.bin_day = False
-            else:
-                pause.minutes(30)
-                continue  
-          
-            tomorrow = (datetime.now() + timedelta(days=1)).replace(
-                hour=6, minute=10, second=0, microsecond=0
-            )
-            logging.info(f"waiting till {tomorrow} to run again")
-            pause.until(tomorrow)
+        if collections:
+            next_collection = collections[0]
+            is_bin_day = self.__is_today_bin_day(next_collection)
+            payload = json.dumps({
+                "date": next_collection.date.strftime("%d/%m/%Y"),
+                "bin_day": is_bin_day,
+                "bin_type": next_collection.wheelie.bin_type
+            })
+            self.__send_message(payload)
+
+    def __send_message(self, payload):
+        broker = "broker"
+        topic = "application/bindicator"
+
+        client = paho.Client(client_id="bindicator")
+        client.connect(broker)
+        client.publish(topic, payload)
 
     def __is_today_bin_day(self, collection: CollectionDate) -> bool:
         return collection.date.date() == datetime.now().date()
 
-    def __turn_lights_off(self):
-        self.bin_day_led.off()
-        self.recycling_bin_led.off()
-        self.rubbish_bin_led.off()
-
-    def __configure_for_bin_day(self, collection: CollectionDate):
-        self.bin_day_led.on()
-        self.bin_day = True
-        self.bin_type = collection.wheelie.bin_type
-        self.__turn_on_bin_light(collection)
-
-    def __turn_on_bin_light(self, collection: CollectionDate):
-        if collection.wheelie.bin_type == "Recycing":
-            self.recycling_bin_led.on()
-        else:
-            self.rubbish_bin_led.on()
-
 
 if __name__ == "__main__":
     args = common_setup()
-    logging.warning("starting Harlow Bindicator")
-    bindicator = Bindicator(args.uprn,args.phone_numbers)
+    logging.info("starting Harlow Bindicator")
+    bindicator = Bindicator(args.uprn)
     bindicator.run()
